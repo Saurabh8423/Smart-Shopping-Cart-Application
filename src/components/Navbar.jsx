0a15008842f5400dd3059a8220e72557f95b2c3c
saurabh8423 <saurabhch9369@gmail.com>
2025-08-19 19:10:56 +0530

    create Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { useSelector } from "react-redux";
+import { Link, NavLink } from "react-router-dom";
+import {selectTotalItems} from "../redux/cartSlice";
+import {useUser} from "../context/UserContext"
 
-const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
+
+export default function Navbar(){
+  const totalItems = useSelector(selectTotalItems)
+  const {user}= useUser()
+  return(
+    <header className="navbar">
+      <link to="/" className="brand">🛍️ SmartCart</link>
+      <nav className="nav">
+        <NavLink to="/" end>Home</NavLink>
+        <NavLink to="/cart">Cart ({totalItems})</NavLink>
+        <NavLink to="/profile">Profile</NavLink>
+      </nav>
+      <div className="user">Hello,<strong>{user.username}</strong></div>
+    </header>
   )
 }
-
-export default Navbar
