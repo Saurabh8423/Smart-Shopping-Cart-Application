396fe26c30ac9e27727cae5f489fa3d6d619679d
saurabh8423 <saurabhch9369@gmail.com>
2025-08-19 21:12:20 +0530

    create store and cartSlice

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import { createSlice, createSelector } from '@reduxjs/toolkit'
 
-const cartSlice = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+// Item shape: { id, name, price, image, qty }
+const initialState = {
+    items: []
 }
 
-export default cartSlice
+const slice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        addToCart: (state, action) => {
+            const item = action.payload
+            const existing = state.items.find((i) => i.id === item.id)
+            if (existing) {
+                existing.qty += 1
+            } else {
+                state.items.push({ ...item, qty: 1 })
+            }
+        },
+        removeFromCart: (state, action) => {
+            state.items = state.items.filter((i) => i.id !== action.payload)
+        },
+        updateQty: (state, action) => {
+            const { id, qty } = action.payload
+            const it = state.items.find((i) => i.id === id)
+            if (it) it.qty = Math.max(1, qty)
+        },
+        clearCart: (state) => {
+            state.items = []
+        }
+    }
+})
+
+
+export const { addToCart, removeFromCart, updateQty, clearCart } = slice.actions
+export default slice.reducer
+
+
+// Selectors
+export const selectItems = (s) => s.cart.items
+export const selectTotalItems = createSelector([selectItems], (items) =>
+    items.reduce((sum, i) => sum + i.qty, 0)
+)
+export const selectTotalPrice = createSelector([selectItems], (items) =>
+    items.reduce((sum, i) => sum + i.qty * i.price, 0)
+)
+export const isInCart = (id) => (s) => s.cart.items.some((i) => i.id === id)
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,9 @@
-import React from 'react'
+import { configureStore } from '@reduxjs/toolkit'
+import cartReducer from './cartSlice'
 
-const store = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default store
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer
+  }
+})
